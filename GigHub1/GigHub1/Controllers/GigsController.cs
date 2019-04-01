811145e97031474969cd2afa05daf5765cecb2f7
asus <asus@ANTER>
2019-04-01 18:28:18 +0200

    Commit

---
+++ GigHub1/GigHub1/Controllers/GigsController.cs
@@ -0,0 +1,33 @@
+﻿using GigHub1.Models;
+using GigHub1.ViewModel;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHub1.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public GigsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _dbContext.Genres
+            };
+        
+
+            
+
+            return View(viewModel);
+        }
+    }
+}
